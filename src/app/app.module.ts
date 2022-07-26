1a11eabbb487d1abee533971f8f21f8086f7cd06
Eldest Pasirula <aknutman@gmail.com>
2022-07-26 23:52:59 +0700

    initial ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemsComponent } from './items/items.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
+import { ItemsComponent, DetailItemsDialog } from './items/items.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemsComponent],
+  declarations: [AppComponent, ItemsComponent, DetailItemsDialog],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+
+    ReactiveFormsModule,
+
+    FlexLayoutModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl } from '@angular/forms';
+
+export interface ItemElement {
+  itemName: string;
+  purchasePrice: number;
+  sellPrice: number;
+  stockCount: number;
+  fileImage: string;
+}
+
+export interface ItemDialogData {
+  isNew: boolean;
+  item: ItemElement;
+}
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.css'],
 })
 export class ItemsComponent implements OnInit {
+  displayedColumns: string[] = [
+    'itemName',
+    'purchasePrice',
+    'sellPrice',
+    'stockCount',
+    'fileImage',
+  ];
+  datasource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.datasource.filter = filterValue.trim().toLowerCase();
   }
 
+  openDialog(isNew: boolean, event?: Event) {
+    const dialogRef = this.dialog.open(DetailItemsDialog, {
+      data: {
+        isNew: isNew,
+        item: event,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+
+const ELEMENT_DATA: ItemElement[] = [
+  {
+    itemName: 'First Item',
+    purchasePrice: 100.2,
+    sellPrice: 1000.1,
+    stockCount: 100,
+    fileImage: 'here',
+  },
+];
+
+@Component({
+  selector: 'detail-items-dialog',
+  templateUrl: 'detail-items.component.html',
+  styleUrls: ['./items.component.css'],
+})
+export class DetailItemsDialog {
+  itemForm = new FormGroup({
+    itemName: new FormControl(''),
+    purchasePrice: new FormControl(0),
+    sellPrice: new FormControl(0),
+    stockCount: new FormControl(0),
+  });
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ItemDialogData) {}
+
+  ngOnInit() {
+    if (typeof this.data.item !== 'undefined') {
+      this.itemForm.patchValue({
+        itemName: this.data.item.itemName,
+        purchasePrice: this.data.item.purchasePrice,
+        sellPrice: this.data.item.sellPrice,
+        stockCount: this.data.item.stockCount,
+      });
+    }
+
+    if (!this.data.isNew) {
+      this.itemForm.controls['itemName'].disable();
+    }
+  }
 }
